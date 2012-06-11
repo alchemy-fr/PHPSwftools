9098e68ece06137224913bd96a89674383d592b3
Romain Neutron <imprec@gmail.com>
2012-06-11 19:02:05 +0200

    Update SwfTools

---
+++ src/SwfTools/Exception/LogicException.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of PHP-SwfTools.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwfTools\Exception;
+
+/**
+ * @author Romain Neutron imprec@gmail.com
+ */
+class LogicException extends \LogicException implements Exception
+{
+
+}
