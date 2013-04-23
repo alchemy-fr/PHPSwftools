db67f348c01d18a75bab26461780f4c2a990f0af
Romain Neutron <imprec@gmail.com>
2013-04-23 12:47:09 +0200

    Update ServiceProvider

--- src/SwfTools/SwfToolsServiceProvider.php
+++ src/SwfTools/SwfToolsServiceProvider.php
@@ -11,6 +11,8 @@
 
 namespace SwfTools;
 
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use SwfTools\Configuration;
@@ -24,29 +26,38 @@ class SwfToolsServiceProvider implements ServiceProviderInterface
         $app['swftools.options'] = array();
 
         $optionsResolver = function($options) {
-                return array_filter(array_replace(
-                    array(
-                        'pdf2swf'    => '',
-                        'swfrender'  => '',
-                        'swfextract' => '',
-                    ), $options
-                ), function($value) {
-                    return $value !== '';
-                });
-            };
+            return array_filter(array_replace(
+                array(
+                    'pdf2swf'    => '',
+                    'swfrender'  => '',
+                    'swfextract' => '',
+                ), $options
+            ), function($value) {
+                return $value !== '';
+            });
+        };
 
-        $app['swftools.pdf-file'] = $app->share(function(Application $app) use ($optionsResolver) {
-            $app['swftools.options'] = $optionsResolver($app['swftools.options']);
-            $logger = isset($app['monolog']) ? $app['monolog'] : null;
+        $app['swftools.logger'] = $app->share(function (Application $app) {
+            if (isset($app['monolog'])) {
+                return $app['monolog'];
+            }
 
-            return new PDFFile(new Configuration($app['swftools.options']), $logger);
+            $logger = new Logger('swftools-logger');
+            $logger->pushHandler(new NullHandler());
+
+            return $logger;
         });
 
-        $app['swftools.flash-file'] = $app->share(function(Application $app) use ($optionsResolver) {
-            $app['swftools.options'] = $optionsResolver($app['swftools.options']);
-            $logger = isset($app['monolog']) ? $app['monolog'] : null;
+        $app['swftools.configuration'] = $app->share(function (Application $app) use ($optionsResolver) {
+            return new Configuration($optionsResolver($app['swftools.options']));
+        });
 
-            return new FlashFile(new Configuration($app['swftools.options']), $logger);
+        $app['swftools.pdf-file'] = $app->share(function(Application $app) {
+            return new PDFFile($app['swftools.configuration'], $app['swftools.logger']);
+        });
+
+        $app['swftools.flash-file'] = $app->share(function(Application $app) {
+            return new FlashFile($app['swftools.configuration'], $app['swftools.logger']);
         });
     }
 
