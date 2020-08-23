f6df94a383ade81c8fd2da552703369a5067d8d9
William Desportes <williamdes@wdes.fr>
2020-08-23 21:07:15 +0200

    Use Doctum instead of Sami

--- buildAPI.sh
+++ buildAPI.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -Rf docs/source/API/API
-/usr/bin/env php vendor/bin/sami.php update sami_configuration.php -v
+composer global require code-lts/doctum:^5.0
+/usr/bin/env php $(composer global config home)/vendor/bin/doctum.php update doctum_configuration.php -v
 sh -c "cd docs && make clean && make html"
