f80db578fb93c39c999ba657064d0aacd729a054
William Desportes <williamdes@wdes.fr>
2020-08-24 14:58:52 +0200

    Fix build command

--- buildAPI.sh
+++ buildAPI.sh
@@ -6,7 +6,8 @@ git add -A docs/
 git commit -m "Removed documentation"
 composer global require code-lts/doctum:^5.0
 /usr/bin/env php $(composer global config home)/vendor/bin/doctum.php update doctum_configuration.php -v
-mv docs/build/API/API docs/source/API/API
+mkdir -p docs/source/API/API
+mv docs/build/API/API/* docs/source/API/API/
 sh -c "cd docs && make clean && make html"
 git add -A docs/
 git commit -m "Updated documentation" --amend
